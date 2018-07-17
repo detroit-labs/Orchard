9c631048ccfad5d8565a8b98fe06148aac895beb
Jeff Kelley <SlaunchaMan@gmail.com>
2018-07-17 10:24:17 -0400

    Add all iPhones and iPads

--- Orchard/iPads.swift
+++ Orchard/iPads.swift
@@ -1,4 +1,6 @@
 //
+// This file was automatically generated and should not be edited.
+//
 //  iPads.swift
 //  Orchard
 //
@@ -6,3 +8,55 @@
 //
 
 import Foundation
+
+public enum iPads {
+
+    @available (iOS, introduced: 3.2, deprecated: 6.0)
+    case iPad
+
+    @available (iOS, introduced: 4.3, deprecated: 10.0)
+    case iPad2
+
+    @available (iOS, introduced: 5.1, deprecated: 10.0)
+    case iPad3
+
+    @available (iOS, introduced: 6.0, deprecated: 11.0)
+    case iPad4
+
+    @available (iOS, introduced: 3.0, deprecated: 10.0)
+    case iPadMini
+
+    @available (iOS, introduced: 7.0)
+    case iPadAir
+
+    @available (iOS, introduced: 7.0)
+    case iPadMini2
+
+    @available (iOS, introduced: 8.1)
+    case iPadAir2
+
+    @available (iOS, introduced: 8.0)
+    case iPadMini3
+
+    @available (iOS, introduced: 9.0)
+    case iPadMini4
+
+    @available (iOS, introduced: 9.1)
+    case iPadPro12_9Inch
+
+    @available (iOS, introduced: 9.3)
+    case iPadPro9_7Inch
+
+    @available (iOS, introduced: 10.3)
+    case iPad5
+
+    @available (iOS, introduced: 10.3)
+    case iPadPro12_9Inch2
+
+    @available (iOS, introduced: 10.3)
+    case iPadPro10_5Inch
+
+    @available (iOS, introduced: 11.3)
+    case iPad6
+
+}
--- Orchard/iPhones.swift
+++ Orchard/iPhones.swift
@@ -17,6 +17,51 @@ public enum iPhones {
     @available (iOS, introduced: 2.0, deprecated: 4.3)
     case iPhone3G
 
+    @available (iOS, introduced: 3.0, deprecated: 7.0)
+    case iPhone3GS
+
+    @available (iOS, introduced: 4.0, deprecated: 8.0)
+    case iPhone4
+
+    @available (iOS, introduced: 5.0, deprecated: 10.0)
+    case iPhone4S
+
+    @available (iOS, introduced: 6.0, deprecated: 11.0)
+    case iPhone5
+
+    @available (iOS, introduced: 7.0, deprecated: 11.0)
+    case iPhone5c
+
+    @available (iOS, introduced: 7.0)
+    case iPhone5s
+
+    @available (iOS, introduced: 8.0)
+    case iPhone6
+
+    @available (iOS, introduced: 8.0)
+    case iPhone6Plus
+
+    @available (iOS, introduced: 9.0)
+    case iPhone6s
+
+    @available (iOS, introduced: 9.0)
+    case iPhone6sPlus
+
+    @available (iOS, introduced: 9.3)
+    case iPhoneSE
+
+    @available (iOS, introduced: 10.0)
+    case iPhone7
+
+    @available (iOS, introduced: 10.0)
+    case iPhone7Plus
+
+    @available (iOS, introduced: 11.0)
+    case iPhone8
+
+    @available (iOS, introduced: 11.0)
+    case iPhone8Plus
+
     @available (iOS, introduced: 11.0)
     case iPhoneX
 
